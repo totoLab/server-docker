7ca3c1dffef285bde4e9dd02e20a3a14b7d08a21
toto <antoniolabate19@gmail.com>
2024-11-05 18:28:59 +0100

    refactoring

--- backrest/scripts/tg_notify.sh
+++ backrest/scripts/tg_notify.sh
@@ -1,2 +1,17 @@
 URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
-curl -s -X POST $URL -d chat_id=$CHAT_ID -d text="$1 $2"
+
+# Assign descriptive names to the input arguments
+PLAN_ID="$1"
+MESSAGE="$2"
+
+# Construct the text based on success condition
+if [[ "$MESSAGE" == *"snapshot success"* ]]; then
+  text="*${PLAN_ID}* ✅
+${MESSAGE}"  # Add checkmark for success
+else
+  text="*${PLAN_ID}*
+${MESSAGE}"  # Only make plan ID bold for other messages
+fi
+
+# Send the message
+curl -s -X POST $URL -d chat_id=$CHAT_ID -d parse_mode="Markdown" -d text="$text"
